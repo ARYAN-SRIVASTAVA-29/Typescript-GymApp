ab0538f04b6427040c4c00676ac01da7812194cb
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-02-11 22:32:52 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits"; 
-import OurClasses from "@/scenes/ourclasses";
+import OurClasses from "@/scenes/ourClasses";
 import ContactUs from "@/scenes/contactUs";
 import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
